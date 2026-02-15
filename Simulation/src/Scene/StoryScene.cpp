a29a67c4a894ce4bea6b87cbc7d2a988fbd32047
kitamoo <ayaka20111005@yahoo.co.jp>
2026-02-15 09:53:41 +0900

    ifflagの処理順を変更

--- Simulation/src/Scene/StoryScene.cpp
+++ Simulation/src/Scene/StoryScene.cpp
@@ -147,10 +147,6 @@ void StoryScene::onEnterNode(const ScriptNode& node)
 {
     currentNode_ = &node;
 
-    if (!node.setFlag.empty())
-    {
-        storyFlags_[node.setFlag] = true;
-    }
     if (!node.ifFlag.empty())
     {
         if (storyFlags_[node.ifFlag])
@@ -163,6 +159,10 @@ void StoryScene::onEnterNode(const ScriptNode& node)
                 return;
             }
         }
+    }
+    if (!node.setFlag.empty())
+    {
+        storyFlags_[node.setFlag] = true;
     }
 }
 
